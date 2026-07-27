b6e64ee8c9fe2c6b2eb54c3d7f2cc60abf3b0e67
sema5056 <rdilmukhamedova@gmail.com>
2026-07-27 17:35:41 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,21 @@
 def calculate(a,operation,b):
   if operation=='+':
     answer=a+b
+    return answer
   elif operation=='-':
     ansawer=a-b
+    return answer
   elif operation=='*':
     answer=a*b
+    return answer
   elif operation=='/' and b==0:
     answer="you can't divide by 0"
+    return answer
   elif operation=="/" and b!=0:
     answer=a/b
+    return answer
   else:
-    answer="aise ValueError"
+    raise ValueError("Unknown operation")"
 if __name__ == "__main__":
   print(calculate(2,+,3))
   print(calculate(7,-,2))
